42fb0112976ccffc8103feffca6c0a99bd116a29
Kgl-01 <karthikgowda.mailme@gmail.com>
2023-01-12 12:37:28 +0530

    created cart-item component and connected our cart drpodown with cartItems reducer

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -15,6 +15,7 @@
 // };
 
 // export default userReducer;
+import { UserActionTypes } from "./user.types";
 
 const INITIAL_STATE = {
   currentUser: null,
@@ -22,7 +23,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
